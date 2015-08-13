7bd68fa5ebc5d52cefc43b324d4f199cb0ed9dcd
theabuitendyk <theabuitendyk@users.noreply.github.com>
2015-08-13 13:43:31 -0700

    broke pause

---
+++ assets/player.js
@@ -0,0 +1,20 @@
+// function playerMovement() {
+//   player.body.velocity.x = 0;
+
+//   if (cursors.left.isDown)
+//   {
+//     player.body.velocity.x = (-200 - playerSpeed);
+
+//     player.animations.play('left');
+//   }
+//   else if (cursors.right.isDown)
+//   {
+//     player.body.velocity.x = (200 + playerSpeed);
+
+//     player.animations.play('right');
+//   }
+//   else
+//   {
+//     player.animations.stop();
+//   }
+// }
